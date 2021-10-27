5345b110923314b429d7c9278c1b61f4bd3ef797
PavanKuchana <Pavankuchana007@gmail.com>
2021-10-27 13:01:43 +0000

    Prime Video React JS/React Slick/React Popup/React Video Player

---
+++ src/setupTests.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+import '@testing-library/jest-dom'
+import {configure} from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    }
+  }
+
+configure({adapter: new Adapter()})
